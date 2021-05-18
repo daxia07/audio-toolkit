d2f7ce06e57383afb8ac0cd6143a24ba649dcc8f
alienz <limingxia07@gmail.com>
2021-05-18 21:07:30 +1000

    init

---
+++ MusicExplorer/__init__.py
@@ -0,0 +1,26 @@
+import logging
+import json
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    try:
+        req_body = req.get_json()
+    except ValueError:
+        pass
+    else:
+        accessToken = req_body.get('accessToken')
+
+    if accessToken:
+        return func.HttpResponse(
+            json.dumps({"data": f"Access Token received as {accessToken}"}),
+             status_code=200,
+             mimetype="application/json"
+        )
+    else:
+        return func.HttpResponse(
+             json.dumps({"data": f"No token found"}),
+             status_code=400,
+             mimetype="application/json",
+        )
