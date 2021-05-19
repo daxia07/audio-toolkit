a51381b64f5adcecaa07ffa65336da87d28a9014
alienz <limingxia07@gmail.com>
2021-05-19 11:31:58 +1000

    use redis to fetch creds

--- MusicExplorer/google_driver.py
+++ MusicExplorer/google_driver.py
@@ -1,6 +1,6 @@
 from googleapiclient import discovery
 from httplib2 import Http
-from oauth2client import file, client, tools
+from oauth2client import client
 
 SCOPES = 'https://www.googleapis.com/auth/drive'
 
@@ -11,17 +11,21 @@ def read_storage_creds():
     return content
 
 
+def get_creds_from_redis(r):
+    return r.get('creds')
+
+
 def save_creds_to_redis(r, data):
-    pass
+    r.set('creds', data)
 
 
-def get_creds():
-    creds = client.Credentials.new_from_json(read_storage_creds())
+def get_creds(r):
+    creds = client.Credentials.new_from_json(get_creds_from_redis(r))
     if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets('client_id.json', SCOPES)
-        store = file.Storage('storage.json')
-        creds = tools.run_flow(flow, store)
-        # save to redis
+        raise ValueError("No valid credentials found to grant access")
+        # flow = client.flow_from_clientsecrets('client_id.json', SCOPES)
+        # store = file.Storage('storage.json')
+        # creds = tools.run_flow(flow, store)
     return creds
 
 
@@ -29,12 +33,26 @@ def get_drive(creds):
     return discovery.build('drive', 'v3', http=creds.authorize(Http()))
 
 
-def get_file_list():
-    creds = get_creds()
+def get_file_list(r):
+    creds = get_creds(r)
     DRIVE = get_drive(creds)
     files = DRIVE.files().list().execute().get('files', [])
     for f in files:
         print(f['name'], f['mimeType'])
 
 
-get_file_list()
+def init_creds():
+    import json
+    import redis
+    with open('../local.settings.json') as f:
+        settings = json.load(f)['Values']
+    r = redis.Redis(host=settings["REDIS_HOST"], port=settings["REDIS_PORT"], password=settings["REDIS_PASS"])
+    # secrets = read_storage_creds()
+    # save_creds_to_redis(r, secrets)
+    return r
+
+
+if __name__ == '__main__':
+    r = init_creds()
+    get_file_list(r)
+    print('OK')
---
+++ readme.md
@@ -0,0 +1,3 @@
+### Remote part to scan google files
+### Read redis to get creds
+### Build update react page to upload repo
